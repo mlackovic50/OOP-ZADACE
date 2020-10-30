04d6a4066d22043d9c3e401a9484708cc0ad20bc
mlackovic50 <72917990+mlackovic50@users.noreply.github.com>
2020-10-30 18:43:59 +0100

    Add files via upload

---
+++ DZ1/ClassLibrary/Episode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Episode
+        {
+            public int viewers;
+            public double sumOfRatings;
+            public double maximumRating = 0;
+
+            public Episode()
+            {
+            }
+
+            public Episode(int aViewers, double aSumOfRatings, double aMaximumRating)
+            {
+                viewers = aViewers;
+                sumOfRatings = aSumOfRatings;
+                maximumRating = aMaximumRating;
+            }
+            public void AddView(double aRandomRating)
+            {
+                double randomRating = aRandomRating;
+                sumOfRatings += randomRating;
+                if (randomRating > maximumRating)
+                {
+                    maximumRating = randomRating;
+                }
+            }
+            public double GetAverageScore()
+            {
+                return sumOfRatings / viewers;
+            }
+            public double GetMaxScore()
+            {
+                return maximumRating;
+            }
+            public double GetViewerCount()
+            {
+                viewers = 10;
+                return viewers;
+
+            }
+        }
+}
